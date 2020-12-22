79fd00bdca90c361d92938f344ede4c8af664b39
giacomo9999 <gmarchesi888@gmail.com>
2020-12-22 17:06:58 -0500

    Added dynamic assignment of ingredients.

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map((elem) => {
+    return [...Array(props.ingredients[elem])].map((_, index) => (
+      <BurgerIngredient key={index} type={elem} />
+    ));
+  });
+  console.log(transformedIngredients);
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
