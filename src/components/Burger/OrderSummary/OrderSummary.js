6951c8eb3c2cfc13c9c5d4a4e3be21b56f6b2ce5
giacomo9999 <gmarchesi888@gmail.com>
2021-01-29 16:48:36 -0500

    Fixed issue with OrderSummary not displaying ingredient counts.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,23 +4,26 @@ import Button from "../../UI/Button/Button";
 
 class OrderSummary extends Component {
   componentDidUpdate() {
-    console.log("Order summary did update.");
+    console.log("Order summary did update...", this.props.ingredients);
   }
 
-  ingredientSummary = Object.keys(this.props.ingredients).map((elem, index) => (
-    <li key={index}>
-      <span style={{ textTransform: "capitalize" }}>
-        {elem} : {this.props.ingredients[elem]}
-      </span>
-    </li>
-  ));
-
   render() {
+    let ingredientSummary = Object.keys(this.props.ingredients).map(
+      (elem, index) => {
+        return (
+          <li key={index}>
+            <span style={{ textTransform: "capitalize" }}>
+              {elem} : {this.props.ingredients[elem]}
+            </span>
+          </li>
+        );
+      }
+    );
     return (
       <Aux>
         <h3>YOUR ORDER</h3>
         <p>A Burger With The Following Ingredients:</p>
-        <ul>{this.ingredientSummary}</ul>
+        <ul>{ingredientSummary}</ul>
         <h3>{`TOTAL PRICE: ${this.props.totalPrice.toFixed(2)}`}</h3>
         <p>Continue To Checkout?</p>
         <Button btnType="Success" clicked={this.props.cancelClick}>
