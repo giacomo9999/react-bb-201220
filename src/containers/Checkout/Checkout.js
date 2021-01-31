3a73dc706665ea5b1cdeafc99805d4cd7efba6af
giacomo9999 <gmarchesi888@gmail.com>
2021-01-31 16:04:06 -0500

    Built ContactData Component.

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
   state = { ingredients: { salad: 1, meat: 1, bacon: 1, cheese: 1 } };
@@ -7,7 +9,6 @@ class Checkout extends Component {
   componentDidMount() {
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
-    console.log("Checkout query: ", query.entries());
     for (let param of query.entries()) {
       ingredients[param[0]] = +param[1];
     }
@@ -29,6 +30,10 @@ class Checkout extends Component {
           ingredients={this.state.ingredients}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route
+          path={this.props.match.path + "/contact-data"}
+          component={ContactData}
         />
       </div>
     );
